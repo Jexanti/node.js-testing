857dced4d827377d95e7f3535367731d199b13f7
Jexanti <76551875+Jexanti@users.noreply.github.com>
2021-01-12 11:05:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
 //var randomnumber = Math.floor(Math.random() * (5 - 0 + 1)) + 0
 //function function1() {
-  //  console.log(randomnumber)
+  //  message.channel.send(randomnumber)
 	//	if (randomnumber = 0) {
-	//		console.log('Command run: f/hello with response 0')
+	//		message.channel.send('Command run: f/hello with response 0')
 	//	}
 	//	if (randomnumber = 1) {
-	//		console.log('Command run: f/hello with response 1')
+	//		message.channel.send('Command run: f/hello with response 1')
 	//	}
 	//	if (randomnumber = 2) {
-	//		console.log('Command run: f/hello with response 2')
+	//		message.channel.send('Command run: f/hello with response 2')
 	//	}
 	//	if (randomnumber = 3) {
-	//		console.log('Command run: f/hello with response 3')
+	//		message.channel.send('Command run: f/hello with response 3')
 	//	}
 	//	if (randomnumber = 4) {
-	//		console.log('Command run: f/hello with response 4')
+	//		message.channel.send('Command run: f/hello with response 4')
 	//	}
 	//	if (randomnumber = 5) {
-	//		console.log('Command run: f/hello with response 5')
+	//		message.channel.send('Command run: f/hello with response 5')
     //  }
 //}
 //function1();
-function func1() {
-    console.log("1");
+function hellorep1() {
+    message.channel.send("1");
+    console.log('Command run: f/hello with response 1')
  }
  
- function func2() {
-    console.log("2");
+ function hellorep2() {
+    message.channel.send("2");
+    console.log('Command run: f/hello with response 2')
  }
  
- function func3() {
-    console.log("3");
+ function hellorep3() {
+    message.channel.send("3");
+    console.log('Command run: f/hello with response 3')
  }
  function random(){
    var i  = Math.floor(Math.random()*20)%4;
    if(i<=0) return random();
    return i;
  }
- function execute(){
+ function execute1(){
    var i = random();
    eval('func'+i+'()');
  }
- execute();
+ execute1();
